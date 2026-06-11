bb8c5580b0616f345bb8a9ae652dc4107f60c2f1
permitchi <mickavallery@gmail.com>
2026-06-12 04:56:30 +0700

    gh pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/stem-turnitin", // Replace with your GitHub repo name
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
